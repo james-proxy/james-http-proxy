7dc21d6ee1bcca3b6a5f84032e4d3d0316bd1664
Mitchell Hentges <mitch9654@gmail.com>
2018-12-22 22:15:03 -0800

    Renames service

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use hyper::service::service_fn;
 
 type BoxFut = Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>;
 
-fn echo(req: Request<Body>) -> BoxFut {
+fn proxy(req: Request<Body>) -> BoxFut {
     let client = Client::new();
     let response_future = client.request(req)
         .and_then(|real_response| {
@@ -25,7 +25,7 @@ fn main() {
     let addr = ([127, 0, 0, 1], 1339).into();
 
     let server = Server::bind(&addr)
-        .serve(|| service_fn(echo))
+        .serve(|| service_fn(proxy))
         .map_err(|e| eprintln!("server error: {}", e));
 
     println!("Running proxy on :1339");
